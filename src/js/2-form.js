7927deb665866def13c6a8c792e6b18f2d907387
Yura Kasyan <33.yurakasyan@gmail.com>
2024-05-12 13:16:05 +0200

    add h/m 09

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const formData = { email: '', message: '' };
+
+const form = document.querySelector('.feedback-form');
+
+const getFormState = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+if (getFormState) {
+  formData.email = getFormState.email;
+  formData.message = getFormState.message;
+
+  form.querySelector('[name="email"]').value = formData.email;
+  form.querySelector('[name="message"]').textContent = formData.message;
+}
+
+form.addEventListener('input', e => {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  if (!formData.email || !formData.message)
+    return alert('Fill please all fields');
+
+  console.log(formData);
+
+  formData.email = '';
+  formData.message = '';
+
+  e.target.elements.message.value = '';
+  e.target.elements.email.value = '';
+
+  localStorage.removeItem('feedback-form-state');
+});
